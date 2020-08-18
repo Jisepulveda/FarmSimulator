ebf46b8c8078c5cf61fa6e8d8e0cd9f4bcd93444
Jisepulveda <jisepulveda@miuandes.cl>
2020-08-18 17:33:29 -0400

    Creacion de proyecto y clase (Map, PrintMap)

---
+++ FarmSimulator/Map.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmSimulator
+{
+    class Map
+    {
+        protected int[,] map = new int[1000,1000];
+
+
+        public void generateMap(bool lake , bool river = true)
+        {
+            //Creamos el mapa con distintos objetos ( piedras, arboles, etc )
+            CreateObjects(this.map);
+
+            if (river == true)
+            {
+                CreateRiver(this.map);
+            }
+
+            if (lake == true)
+            {
+                CreateLake(this.map);
+            }
+
+            CreateFarm(this.map);
+
+        }
+
+        private void CreateRiver(int[,] map)
+        {
+
+        }
+
+        private void CreateLake(int[,] map)
+        {
+
+        }
+
+        private void CreateObjects(int[,] map)
+        {
+
+        }
+
+        private void CreateFarm(int[,] map)
+        {
+
+        }
+    }
+}
